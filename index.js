30512502c0a5960d78aa8cb691d9661f6fd3a0f3
UfukGuler34 <ufuk.gulermega@gmail.com>
2021-10-03 20:59:10 +0300

    day-one

---
+++ index.js
@@ -0,0 +1,33 @@
+// For comment
+// document.getElementById("count-el").innerText = 5;
+
+// Simple Math Ops.
+// let count = 5
+// console.log(count)
+
+// let myAge = 23 
+// console.log(myAge)
+
+// let text = "hello"
+// console.log(text)
+
+// let final = count + myAge
+// console.log(final)
+
+// let myAgetwo = 23 
+// let humanDogRatio = 7
+// let dogAge = myAgetwo * humanDogRatio
+// console.log(dogAge)
+
+// initialize the count 0
+// listen for clicks on the increment and decrement button
+// increase the court variable when the button is clicked
+// change the court-el in the HTML to reflect the new count
+
+let count = 0
+function increase() {
+    console.log("increase button")
+}
+function decrease() {
+    console.log("decrease button")
+}
